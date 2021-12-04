9399d1244ab40c2f07395d47956dbcd820367fd9
y <a@a.com>
2021-12-04 12:46:56 +0900

    acceleration, friction

--- components.js
+++ components.js
@@ -9,8 +9,6 @@ function ComponentBase(inType, inActor) {
     this.type = inType;
     this.actor = inActor;
     // Component.prototype.actor = inActor; // 모든 인스턴스가 공유하는 객체가 된다.
-
-    // console.log(this.type, this.actor);
 }
 
 ComponentBase.prototype.getType = function (inType) {
@@ -34,8 +32,12 @@ function MoveComponent(inActor) {
     ComponentBase.call(this, ComponentType_Move, inActor);
 
     this.position = {x: 0, y: 0}; // todo move to actor
-    this.speedPerSecond = { x: 50, y: 50 };
+    this.speedMax = { x: 6, y: 50 };
     this.speed = { x: 0, y: 0 };
+    this.friction = { x: -70, y: 0 };
+    this.accel = { x: 0, y: 0 };
+    this.accelMax = { x: 90, y: 0 };
+    this.jumpSpeed = { x: 0, y: 50 };
 }
 MoveComponent.prototype = Object.create(ComponentBase.prototype); // 상속
 
@@ -47,7 +49,35 @@ MoveComponent.prototype.update = function (inFramework) {
 
     this.updateSpeedByDirection(inFramework);
 
+    this.speed.x += this.accel.x;
+    this.speed.y += this.accel.y;
+
+    var speedSignBefore = 1;
+    if(0 != this.speed.x)
+    {
+        speedSignBefore = this.speed.x / Math.abs(this.speed.x);
+    }
+
+    this.speed.x += this.friction.x * deltaTime * speedSignBefore;
     this.speed.y += gravity * deltaTime;
+
+    var speedSignAfter = 1;
+    if(0 != this.speed.x)
+    {
+        speedSignAfter = this.speed.x / Math.abs(this.speed.x);
+    }
+
+    if(speedSignBefore != speedSignAfter)
+    {
+        this.speed.x = 0;
+    }
+
+    if(Math.abs(this.speedMax.x) < Math.abs(this.speed.x))
+    {
+        this.speed.x = Math.abs(this.speedMax.x) * speedSignAfter;
+    }
+
+    console.log(this.speed.x);
 
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
@@ -61,24 +91,27 @@ MoveComponent.prototype.updateSpeedByDirection = function (inFramework) {
     // e.keyCode(e.code)는 대소문자 구분없이 같다.
     // A a:65, D d:68, W w:87, S s:83, space:32
 
+    this.accel.x = 0;
+    this.accel.y = 0;
+
     if (inFramework.isPressedKey("A")) {
-        this.speed.x = this.speedPerSecond.x * -1;
+        this.accel.x = this.accelMax.x * -1;
     }
 
     if (inFramework.isPressedKey("D")) {
-        this.speed.x = this.speedPerSecond.x;
+        this.accel.x = this.accelMax.x;
     }
 
     if (inFramework.isPressedKey("W")) {
-        this.speed.y = this.speedPerSecond.y * -1;
+        this.accel.y = this.accelMax.y * -1;
     }
 
     if (inFramework.isPressedKey("S")) {
-        this.speed.y = this.speedPerSecond.y;
+        this.accel.y = this.accelMax.y;
     }
 
-    this.speed.x *= inFramework.getDeltaTime();
-    this.speed.y *= inFramework.getDeltaTime();
+    this.accel.x *= inFramework.getDeltaTime();
+    this.accel.y *= inFramework.getDeltaTime();
 }
 
 MoveComponent.prototype.getPosition = function() {
@@ -108,7 +141,5 @@ ShapeComponent.prototype.render = function (inFramework) {
         // draw
         canvasContext.fillStyle = 'green';
         canvasContext.fillRect(position.x, position.y, this.size.x, this.size.y);
-
-        // console.log(position, this.size);
     }
 }
--- framework.js
+++ framework.js
@@ -6,16 +6,11 @@ function Framework(){
     this.fps = 60;
     this.deltaTime = 1 / this.fps;
 
-
-    this.gravity = 9.8;
+    // this.gravity = 9.8;
+    this.gravity = 0;
     this.actors = [];
 
     this.playerActor = null;
-
-    // this.position = { x: 0, y: 0 };
-    // this.size = { x: 10, y: 10 };
-    // this.speedPerSecond = { x: 50, y: 50 };
-    // this.speed = { x: 0, y: 0 };
 }
 
 Framework.prototype.init = function() {
@@ -73,56 +68,20 @@ Framework.prototype.updateInput = function() {
     var moveComponent = this.playerActor.getComponentByType(ComponentType_Move);
     if(null !== moveComponent)
     {
-        moveComponent.clearSpeed();
         moveComponent.updateSpeedByDirection(this);
     }
-
-    // e.keyCode는 대소문자 구분없이 같다.
-    // A a:65, D d:68, W w:87, S s:83, space:32
-
-    // this.speed.x = 0;
-    // this.speed.y = 0;
-    //
-    // if (this.isPressedKey("A")) {
-    //     speed.x = speedPerSecond.x * -1;
-    // }
-    //
-    // if (this.isPressedKey("D")) {
-    //     speed.x = speedPerSecond.x;
-    // }
-    //
-    // if (this.isPressedKey("W")) {
-    //     speed.y = speedPerSecond.y * -1;
-    // }
-    //
-    // if (this.isPressedKey("S")) {
-    //     speed.y = speedPerSecond.y;
-    // }
-    //
-    // speed.x *= deltaTime;
-    // speed.y *= deltaTime;
 }
 
 Framework.prototype.update = function() {
-    // input
-    // position.x += speed.x;
-    // position.y += speed.y;
-
     this.actors.forEach(actor => actor.updateComponents(this));
 }
 
 Framework.prototype.render = function() {
-    // clear
+    // clear canvas
     this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     var thisFramework = this;
     this.actors.forEach(actor => actor.renderComponents(thisFramework));
-    // // clear
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //
-    // // draw
-    // ctx.fillStyle = 'green';
-    // ctx.fillRect(position.x, position.y, size.x, size.y);
 }
 
 Framework.prototype.gameLoop = function() {
